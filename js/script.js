7e5fdb753982fcc9b7c88dda10fe3b68dbdf48cc
Artur Gręziak <alex4448@wp.pl>
2021-06-21 21:51:08 +0200

    first commit script_js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const sectionCommodity = document.querySelector('.section__commodity');
+const fotProducts = document.querySelector('.foto');
+const price = document.querySelector('.price');
+const buton = document.querySelector('.buton');
+const name = document.querySelector('.name');

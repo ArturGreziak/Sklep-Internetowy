59585114f724ee8fd1e268d2d3ebe50ee063956a
Artur Gręziak <alex4448@wp.pl>
2021-06-22 17:06:15 +0200

    inserting elements

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@ const fotProducts = document.querySelector('.foto');
 const price = document.querySelector('.price');
 const buton = document.querySelector('.buton');
 const name = document.querySelector('.name');
+
+const el = document.querySelector('.products');
+
+const item = [
+  { src: "./img/curso2.jpg", alt: "foto", },
+  { src: "./img/curso2.jpg", alt: "foto", },
+  { src: "./img/curso2.jpg", alt: "foto", },
+];
+
+for (let i = 0; i < item.length; i++)
+{
+
+  el.innerHTML += ` 
+  
+  <div class="section__commodity">
+        <img class="foto" src="./img/curso2.jpg" alt="foto">
+        <h3 class="price">510 $</h3>
+        <button class="buton" id="1">KUP</button>
+        <h4 class="name">Jabłko</h4>
+  </div>
+  
+  `
+}

c6555b4151e4bbbdce2206f8db69e99165bf0ab2
Artur Gręziak <alex4448@wp.pl>
2021-06-22 22:35:40 +0200

    DOM object change

--- js/script.js
+++ js/script.js
@@ -20,15 +20,32 @@ const item = [
 
 for (let i = 0; i < item.length; i++)
 {
-
-  el.innerHTML += ` 
+  let div = document.createElement('div');
+  let img = document.createElement('img');
+  let h3 = document.createElement('h3');
+  let button = document.createElement('button');
+  let h4 = document.createElement('h4');
   
-  <div class="section__commodity">
-        <img class="foto" src=${item[i].src} alt="foto">
-        <h3 class="price">${item[i].class_price}</h3>
-        <button class="buton" id=${item[i].id}>KUP</button>
-        <h4 class="name">${item[i].name}</h4>
-  </div>
+  div.classList.add('section__commodity');
+  img.classList.add('foto');
+  img.src = item[i].src;
+  img.alt = item[i].alt;
+  h3.innerText = item[i].class_price;
+  button.classList.add('buton');
+  button.id = '1';
+  button.innerText = 'KUP';
+  h4.classList.add('name');
+  h4.innerText = `${item[i].name}`;
   
-  `
-}
+  div.appendChild(img);
+  div.appendChild(h3);
+  div.appendChild(button);
+  div.appendChild(h4);
+  
+  el.appendChild(div); 
+}// <div class="section__commodity">
+  //       <img class="foto" src=${item[i].src} alt="foto">
+  //       <h3 class="price">${item[i].class_price}</h3>
+  //       <button class="buton" id=${item[i].id}>KUP</button>
+  //       <h4 class="name">${item[i].name}</h4>
+  // </div>
